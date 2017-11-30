391d4ea3b993f69c79834915d6d79799e0f41f3d
felipegatica <gatica.adrover@gmail.com>
2017-11-29 21:29:15 -0300

    plantilla

---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,147 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+		        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FISCHTEL</title>
+        <!-- css table datatables/dataTables -->
+	<link rel="stylesheet" href="datatables/dataTables.bootstrap.css"/>
+    
+         <link type="text/css" href="css/bootstrap.css" rel="stylesheet">
+        <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
+        <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link type="text/css" href="css/theme.css" rel="stylesheet">
+        <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+        <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600'    rel='stylesheet'>
+            
+            
+            
+        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="navbar navbar-fixed-top">
+           <div class="navbar-inner"> 
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+                        <i class="icon-reorder shaded"></i></a><a class="navbar-brand" href="{{ url('/') }}">FISCHTEL</a>
+           		   
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a href="{{ route('login') }}">Ingresar</a></li>
+                            <li><a href="{{ route('register') }}">Registrarse</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Salir
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
+                    </ul>
+                
+				   
+                </div>
+            </div> 
+            <!-- /navbar-inner -->
+        </div>
+
+           <div class="container">
+                <div class="row">
+                    <div class="span12">
+                        <div class="content">
+                            
+                         
+                            
+                        </div>
+
+      @yield('content')
+                        <!--/.content-->
+                    </div>
+                    <!--/.span9-->
+                </div>
+            </div>
+            <!--/.container-->
+
+   
+        
+        <!--/.wrapper--><br />
+        <div class="footer span-12">
+            <div class="container">
+              <center> <b class="copyright"> FISCHTEL </a> </center>
+            </div>
+        </div>
+
+        <!--
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        
+        <script src="datatables/jquery.dataTables.js"></script>
+        <script src="datatables/dataTables.bootstrap.js"></script>
+        <script>
+        $(document).ready(function() {
+				var dataTable = $('#lookup').DataTable( {
+					
+				 "language":	{
+					"sProcessing":     "Procesando...",
+					"sLengthMenu":     "Mostrar _MENU_ registros",
+					"sZeroRecords":    "No se encontraron resultados",
+					"sEmptyTable":     "Ningún dato disponible en esta tabla",
+					"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+					"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+					"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+					"sInfoPostFix":    "",
+					"sSearch":         "Buscar:",
+					"sUrl":            "",
+					"sInfoThousands":  ",",
+					"sLoadingRecords": "Cargando...",
+					"oPaginate": {
+						"sFirst":    "Primero",
+						"sLast":     "Último",
+						"sNext":     "Siguiente",
+						"sPrevious": "Anterior"
+					},
+					"oAria": {
+						"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+						"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+					}
+				},
+
+					"processing": true,
+					"serverSide": true,
+					"ajax":{
+						url :"ajax-grid-data.php", // json datasource
+						type: "post",  // method  , by default get
+						error: function(){  // error handling
+							$(".lookup-error").html("");
+							$("#lookup").append('<tbody class="employee-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
+							$("#lookup_processing").css("display","none");
+							
+						}
+					}
+				} );
+			} );
+        </script>
+      -->
+    </body>
+
+</html>
