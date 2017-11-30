6738c6545d61852c6f3d0c15cc379116f80338e1
felipegatica <gatica.adrover@gmail.com>
2017-11-29 21:12:42 -0300

    commit

---
+++ resources/views/zyro/1.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Home</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<!-- Facebook Open Graph -->
+	<meta name="og:title" content="Home" />
+	<meta name="og:description" content="" />
+	<meta name="og:image" content="" />
+	<meta name="og:type" content="article" />
+	<meta name="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=20171108105050" type="text/javascript"></script>
+
+	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+	<link href="css/site.css?v=20171116120930" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1510878103" rel="stylesheet" type="text/css" />
+	<link href="css/1.css?ts=1510878103" rel="stylesheet" type="text/css" />
+	{{ga_code}}
+	<script type="text/javascript">var currLang = '';</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body><div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div id="wb_cs_row_23" class="wb-cs-row"><div id="wb_cs_col_24" class="wb-cs-col"><div id="wb_cs_row_25" class="wb-cs-row"><div id="wb_cs_col_26" class="wb-cs-col"><div id="wb_element_instance1" class="wb_element wb_element_picture wb-cs-elem"><a href="Home/"><img alt="gallery/logo_ring" src="gallery_gen/dbf81e0c0cb3649a0bbf276c108ca7e8_64x64.png"></a></div></div><div id="wb_cs_col_27" class="wb-cs-col"><div id="wb_element_instance2" class="wb_element wb-cs-elem" style=" line-height: normal;"><h4 class="wb-stl-pagetitle"><a href="Home/">F</a>ischTel</h4>
+</div></div><div class="wb-cs-clear"></div></div></div><div id="wb_cs_col_28" class="wb-cs-col wb-cs-right"><div id="wb_element_instance0" class="wb_element wb-menu wb-menu-mobile wb-cs-elem"><a class="btn btn-default btn-collapser"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a><ul class="hmenu"><li class="active"><a href="Home/" target="_self" title="Home">Home</a></li><li><a href="https://fischtel.000webhostapp.com/login/index.php" target="_self" title="Usuarios">Usuarios</a></li><li><a href="https://fischtel.000webhostapp.com/login/signup.php" target="_self" title="Registrate">Registrate</a></li></ul><div class="clearfix"></div></div></div><div class="wb-cs-clear"></div></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div id="wb_cs_row_1" class="wb-cs-row"><div id="wb_cs_col_2" class="wb-cs-col"><div id="wb_element_instance12" class="wb_element wb_element_picture wb-cs-elem"><img alt="gallery/big_ring" src="gallery_gen/5652020a1bdb066641b0172598a54c5b_256x256.png"></div></div><div class="wb-cs-clear"></div></div><div id="wb_cs_row_3" class="wb-cs-row"><div id="wb_cs_col_4" class="wb-cs-col"><div id="wb_element_instance11" class="wb_element wb-cs-elem" style=" line-height: normal;"><h5 class="wb-stl-subtitle" style="text-align: center;">Fischtel</h5>
+</div></div><div class="wb-cs-clear"></div></div><div id="wb_cs_row_5" class="wb-cs-row"><div id="wb_cs_col_6" class="wb-cs-col"><div id="wb_element_instance13" class="wb_element wb-cs-elem" style=" line-height: normal;"><p class="wb-stl-normal" style="text-align: center;"><font color="#ffffff">Proyecto piloto para la fiscalización chilena de Telecomunicaciones</font></p>
+</div></div><div class="wb-cs-clear"></div></div><div id="wb_cs_row_7" class="wb-cs-row"><div id="wb_cs_col_8" class="wb-cs-col"><div id="wb_element_instance10" class="wb_element wb-cs-elem"><a class="wb_button" href="https://fischtel.000webhostapp.com/login/index.php"><span>Ingresa</span></a></div></div><div class="wb-cs-clear"></div></div><div id="wb_cs_row_9" class="wb-cs-row"><div id="wb_cs_col_10" class="wb-cs-col"><div id="wb_element_instance14" class="wb_element wb_element_shape wb-cs-elem"><div class="wb_shp"></div><div class="wb-elm-ch-wrp"><div id="wb_cs_row_11" class="wb-cs-row"><div id="wb_cs_col_12" class="wb-cs-col"><div id="wb_element_instance6" class="wb_element wb-cs-elem" style=" line-height: normal;"><h2 class="wb-stl-heading2">Controla tu sistema</h2>
+
+<p class="wb-stl-normal">Administra y reporta nuevos puntos de instalacion.</p>
+</div></div><div id="wb_cs_col_13" class="wb-cs-col"><div id="wb_element_instance7" class="wb_element wb-cs-elem" style=" line-height: normal;"><h2 class="wb-stl-heading2">Contacto</h2>
+
+<p class="wb-stl-normal">Accede rapidamente a tus ingresos para fiscalizaciones más rápidas</p>
+</div></div><div id="wb_cs_col_14" class="wb-cs-col"><div id="wb_element_instance8" class="wb_element wb-cs-elem" style=" line-height: normal;"><h2 class="wb-stl-heading2">Facil</h2>
+
+<p class="wb-stl-normal">Subir puntos de red y nuevas conexiones nunca fue tan facil</p>
+</div></div><div class="wb-cs-clear"></div></div><div id="wb_cs_row_15" class="wb-cs-row"><div id="wb_cs_col_16" class="wb-cs-col"><div id="wb_element_instance4" class="wb_element wb-elm-orient-horizontal wb-cs-elem"><div class="wb-elm-line"></div></div></div><div class="wb-cs-clear"></div></div><div id="wb_cs_row_17" class="wb-cs-row"><div id="wb_cs_col_18" class="wb-cs-col"><div id="wb_element_instance5" class="wb_element wb-cs-elem" style=" line-height: normal;"><h1 class="wb-stl-heading1">Nosotros</h1>
+</div></div><div class="wb-cs-clear"></div></div><div id="wb_cs_row_19" class="wb-cs-row"><div id="wb_cs_col_20" class="wb-cs-col"><div id="wb_element_instance9" class="wb_element wb-cs-elem" style=" line-height: normal;"><h2 class="wb-stl-heading2"><a href="https://fischtel.000webhostapp.com/login/index.php">Somos un grupo de Estudiantes de Ingenieria en Informatica.</a></h2>
+
+<p> </p>
+
+<p class="wb-stl-normal">Nos especializamos en una clase, levantando el proceso completo de un sistema de administracion para redes de telecomunicación, generando sistema eficiente de fácil manejo y administración para la Subtel </p>
+
+<p class="wb-stl-normal"> </p>
+
+<p class="wb-stl-normal">Somos Universidad Central de Chile</p>
+
+<p class="wb-stl-normal"> </p>
+</div></div><div class="wb-cs-clear"></div></div><div class="wb-cs-clear"></div></div></div></div><div class="wb-cs-clear"></div></div><div id="wb_element_instance15" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(1);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance15");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance15").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div>
+<div class="vbox wb_container" id="wb_footer" style="height: 134px;">
+	
+<div id="wb_cs_row_21_wrp" class="wb_cont_inner_rel"><div id="wb_cs_row_21" class="wb-cs-row"><div id="wb_cs_col_22" class="wb-cs-col"><div id="wb_element_instance3" class="wb_element wb-cs-elem" style=" line-height: normal;"><p class="wb-stl-footer"> <a href="http://fischtel.000webhostapp.com">fischtel.000webhostapp.com</a></p>
+</div></div><div class="wb-cs-clear"></div></div><div class="wb-cs-clear"></div></div><div id="wb_element_instance16" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+</html>
